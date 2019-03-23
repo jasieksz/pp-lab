08ad5f1bb7c2ca989c92bb5521f74e5f8ef96eeb
Jasiek Sznajd <jasiek.sznajd@gmail.com>
2019-03-23 21:16:34 +0100

    lab3: Test on jabba done, now running on ZEUS

--- lab3/montecarlo_pi/run.sh
+++ lab3/montecarlo_pi/run.sh
@@ -2,15 +2,30 @@
 
 #SBATCH -N 1
 #SBATCH --cpus-per-task=12
-#SBATCH --time=48:00:00 
+#SBATCH --time=30:00:00 
 #SBATCH -p plgrid
 #SBATCH --output="output.out"
 #SBATCH --error="error.err"
  
 srun /bin/hostname
  
-module load plgrid/tools/python/3.7.2
+module load plgrid/tools/python/2.7.9
+module add plgrid/tools/impi
  
 cd $SLURM_SUBMIT_DIR
  
-python main.py
+for size in {6..8}
+do
+    for proc in {1..12}
+    do
+        mpiexec -np $proc ./py2_pi.py $size regular >> res_reg.txt
+    done
+done
+
+for size in {6..8}
+do
+    for proc in {1..12}
+    do
+        mpiexec -np $proc ./py2_pi.py $size scale >> res_scale.txt
+    done
+done
